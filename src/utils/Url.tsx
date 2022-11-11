1011bbb8587646f1062cf55c79ca15e4c500677e
petar.vasilev0727@gmail.com <petar.vasilev0727@gmail.com>
2022-11-11 20:00:59 +0000

    api url changed

--- src/utils/Url.tsx
+++ src/utils/Url.tsx
@@ -1,3 +1,3 @@
-export const UploadFileToS3Bucket: string = 'http://3.235.141.85:3000/upload_mturk_s3?'
+export const UploadFileToS3Bucket: string = '/api/upload_mturk_s3?'
 
 export const GetFilsFromS3Bucket: string = 'https://assets-bhuman-new.s3.amazonaws.com/'
